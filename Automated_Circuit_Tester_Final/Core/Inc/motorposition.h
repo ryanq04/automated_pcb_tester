d5a8a75131c6a34d4a94a7f2410a93b7ebfdffe4
Ryan <ryan2004.qi@gmail.com>
2025-03-31 16:59:47 -0400

    added flush

--- Automated_Circuit_Tester_Final/Core/Inc/motorposition.h
+++ Automated_Circuit_Tester_Final/Core/Inc/motorposition.h
@@ -44,7 +44,7 @@ extern const Position HOME; // probe home, 3 mm off the ground
 
 // define offsets and distances based on workspace
 #define H 12.5 // Z-axis height from ground
-#define PROBE_LEN 12.3 // need to edit, in mm, length of the probe
+#define PROBE_LEN 12.2 // need to edit, in mm, length of the probe
 #define THETA_MAX 34.0
 #define X_MAX 13.0 // max distance stepper can move
 #define DR_MAX 2.7
